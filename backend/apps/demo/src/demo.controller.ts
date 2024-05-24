22ed779b93e3d27fc78345f5c94c388acf206664
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-05-24 16:13:10 +0500

    first commit

---
+++ backend/apps/demo/src/demo.controller.ts
@@ -0,0 +1,15 @@
+import { Controller } from '@nestjs/common';
+import { DemoService } from './demo.service';
+import { EventPattern, MessagePattern } from '@nestjs/microservices';
+
+@Controller()
+export class DemoController {
+  constructor(private readonly demoService: DemoService) {}
+
+  @MessagePattern('hello')
+  getHello(): string {
+    console.log('hello');
+
+    return this.demoService.getHello();
+  }
+}
