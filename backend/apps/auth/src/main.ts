99da4206e42a0cd3d7c4ef00ff7f1e11d5c91cc1
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-05-31 09:00:14 +0500

    commited

--- backend/apps/auth/src/main.ts
+++ backend/apps/auth/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AuthModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: ['nats://localhost:4222'],
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
